9593ab6a202f2a2347f8f3d6681308136c737658
Daniel <studiosdopplermusic@gmail.com>
2022-10-18 22:01:04 -0500

    paste

--- multicursor/02_multicorsos_basico.js
+++ multicursor/02_multicorsos_basico.js
@@ -4,24 +4,26 @@
 
 ctr + alt + down/up
 
+
+-  Los elementos se pueden copiar multiples
 */ 
 
 console.log('Hola Mundo');
 console.log('Hola Mundo');
 console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
-console.log('Hola Mundo');
+console.log('Hola Mundo'); // test1
+console.log('Hotest2'); // 
+console.log('Hotest3'); // 
+console.log('Hotest4'); // 
+console.log('Hotest5'); // 
+console.log('Hotest6'); // 
+console.log('Hotest6'); // 
+console.log('Hotest7'); // 
+console.log('Hotest7'); // 
+console.log('Hotest8'); // 
+console.log('Hotest9'); // 
+console.log('Hotest9'); // 
+console.log('Hotest0'); // 
+console.log('Hotest0'); // 
+console.log('H testo'); //-
+console.log('Hotest2'); // 
