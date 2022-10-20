9258ccb426e365b168a21e351a54f15e31bee77b
Daniel <studiosdopplermusic@gmail.com>
2022-10-19 21:40:10 -0500

    see propieties

---
+++ definiciones_y_snippets/01_definiciones.ts
@@ -0,0 +1,19 @@
+/*
+
+Ctrl + shift + o (Ver elementos del documento)
+Ctrl + shift + o + : (categoriza por elementos)
+
+
+*/
+
+class Person{
+    constructor(){}
+
+    name:string;
+    phone:number;
+    age:number;
+    
+    calcAge(){
+        return this.age;
+    }
+}
